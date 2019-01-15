81da596228dc821a9ef6a4472795109811ffbad5
root <root@localhost.localdomain>
2019-01-15 16:59:26 +0000

    Layer 2 flag - final version

--- L2Flag2.py
+++ L2Flag2.py
@@ -8,8 +8,8 @@ def check_eth0_status():
     return str(subprocess.check_output("cat /sys/class/net/eth0/operstate", shell=True)).strip()
 
 def main():
+    flag = "flag_{I7_W45_R34lly_Impr3551v3}"
     while check_eth0_status() == "up":
-        flag = "flag_{I7_W45_R34lly_Impr3551v3}"
         for i in range(5):
             sendp(Ether(src='5e:c1:5c:00:13:57', dst='af:1a:91:5c:10:5e', type=0x88e5)/flag)
         time.sleep(5)
